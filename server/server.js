325aaabcf56860ea4319f8074a3b57102c5f671c
beridzezura14 <zuraberidze12@gmail.com>
2025-09-27 17:59:58 +0400

    commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const authRoute = require("./routes/auth");
 const wishesRoute = require("./routes/wishes");
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
+
+// CommonJS-ში __dirname უკვე ხელმისაწვდომია, fileURLToPath საჭირო აღარ არის
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
@@ -17,15 +20,13 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", authRoute);
 app.use("/api/wishes", wishesRoute);
 
-
-
+// Serve React build
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // Catch-all route for React Router
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
