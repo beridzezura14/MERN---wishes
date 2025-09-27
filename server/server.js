8f7e6a9f06e82c4130ed42478a2b320c754f17b8
beridzezura14 <zuraberidze12@gmail.com>
2025-09-27 17:50:16 +0400

    commit

--- server/server.js
+++ server/server.js
@@ -17,5 +17,15 @@ mongoose.connect(process.env.MONGO_URI)
 app.use("/api/auth", authRoute);
 app.use("/api/wishes", wishesRoute);
 
+
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// Catch-all route for React Router
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+});
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
